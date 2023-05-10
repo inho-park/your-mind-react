d54ec776af290dc2a458c9040c9339c9612472b7
JunSeong_Park <ttotto0608@naver.com>
2023-05-10 15:37:04 +0900

    receive emotion data

--- src/pages/ipc/IpcRender.js
+++ src/pages/ipc/IpcRender.js
@@ -1,24 +1,33 @@
+// import IpcComponent from "./components/IpcComponent";
 import { useState } from "react";
 const electron = window.require('electron');
 const { ipcRenderer } = electron;
 
-
 const file_path = {
-  webm : "./python_src/video/test4.webm",
-  mp4 : "./python_src/video/test4.mp4"
+    webm : "./python_src/video/test4.webm",
+    mp4 : "./python_src/video/test4.mp4"
 }
 
-export default function IpcRenderer(props) {
-  const [message, setMessage] = useState('');
+ipcRenderer.send("convert script", JSON.stringify(file_path));
 
-  ipcRenderer.send("convert script", JSON.stringify(file_path));
+
+export default function IpcRenderer() {
+  
+  const [message, setMessage] = useState('');
+  
   ipcRenderer.on("convert success",(event, arg) => {
-      setMessage(arg);
+    const data = JSON.stringify(arg);  
+    setMessage(data);
   });
+
   return (
-      <div>
-        <h1>{message}</h1>
-      </div>
-    );
+    <div>
+      {/* <IpcComponent
+          webm = {file_path.webm}
+          mp4 = {file_path.mp4}/> */}
+
+      <h2>{message}</h2>
+    </div>
+  );
 }
   
